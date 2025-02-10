c600a91bb79cb261764911dae46aea990e199196
cheatkurtl <leo.maier2008@gmail.com>
2025-02-10 20:21:22 +0100

    settings menu

--- core/src/main/java/at/peckventure/menu/MainMenu.java
+++ core/src/main/java/at/peckventure/menu/MainMenu.java
@@ -1,6 +1,5 @@
 package at.peckventure.menu;
 
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -13,7 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-
 
 public class MainMenu implements Screen {
     private Game game;
@@ -31,20 +29,20 @@ public class MainMenu implements Screen {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
-        // 🎨 Eigenes Hintergrundbild
+        // Hintergrund
         backgroundTexture = new Texture("textures/background/forest.png");
         backgroundImage = new Image(backgroundTexture);
         backgroundImage.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         stage.addActor(backgroundImage);
 
-        // 📌 Eigenes Label für den Titel
+        // Titel
         Label.LabelStyle titleStyle = new Label.LabelStyle();
-        titleStyle.font = new BitmapFont(); // Nutzt Standard-Font, kann ersetzt werden
+        titleStyle.font = new BitmapFont();
         titleLabel = new Label("Peckventure", titleStyle);
         titleLabel.setFontScale(2f);
         titleLabel.setAlignment(Align.center);
 
-        // 🔘 Eigene Buttons mit Texturen
+        // Button-Stil
         Texture buttonTexture = new Texture("textures/gui/button1.png");
         TextureRegionDrawable buttonDrawable = new TextureRegionDrawable(buttonTexture);
         TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
@@ -52,6 +50,7 @@ public class MainMenu implements Screen {
         buttonStyle.down = buttonDrawable;
         buttonStyle.font = new BitmapFont();
 
+        // Buttons
         TextButton singlePlayerButton = new TextButton("SinglePlayer", buttonStyle);
         TextButton multiPlayerButton = new TextButton("MultiPlayer", buttonStyle);
         TextButton creditsButton = new TextButton("Credits", buttonStyle);
@@ -60,15 +59,14 @@ public class MainMenu implements Screen {
 
         Table rootTable = new Table();
         rootTable.setFillParent(true);
+        Table buttonTable = new Table();
 
-        Table buttonTable = new Table(); // Extra Tabelle nur für die Buttons
-
-        // 📌 Titel oben anheften
+        // Titel
         rootTable.top();
-        rootTable.add(titleLabel).padTop(50).expandX().center(); // Oben, mittig zentriert
+        rootTable.add(titleLabel).padTop(50).expandX().center();
         rootTable.row();
 
-        // 📌 Buttons in eine eigene Tabelle packen, die sich mittig ausrichtet
+        // Buttons platzieren
         buttonTable.center();
         buttonTable.add(singlePlayerButton).size(300, 80).pad(20);
         buttonTable.row();
@@ -78,38 +76,39 @@ public class MainMenu implements Screen {
         buttonTable.row();
         buttonTable.add(settingsButton).size(300, 80).pad(20);
         buttonTable.row();
-        buttonTable.add(settingsButton).size(300, 80).pad(20);
-        buttonTable.row();
         buttonTable.add(exitButton).size(300, 80).pad(20);
 
-        // 📌 Buttons mittig setzen
         rootTable.add(buttonTable).expandY().center();
-
         stage.addActor(rootTable);
 
-
-        // Button Listeners
+        // Button Events
         singlePlayerButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new SinglePlayer(game)); // Screen-Wechsel zu GameScreen
+                game.setScreen(new SinglePlayer(game));
+            }
+        });
+
+        settingsButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new Settings(game)); // 🎯 Geht zu Settings
             }
         });
 
         exitButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                Gdx.app.exit(); // 📌 Spiel beenden
+                Gdx.app.exit();
             }
         });
 
         multiPlayerButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new MultiPlayer(game)); // Screen-Wechsel zu GameScreen
+                game.setScreen(new MultiPlayer(game));
             }
         });
-
     }
 
     @Override
@@ -120,28 +119,16 @@ public class MainMenu implements Screen {
     }
 
     @Override
-    public void resize(int i, int i1)
-    {
-
-    }
+    public void resize(int i, int i1) {}
 
     @Override
-    public void pause()
-    {
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume()
-    {
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide()
-    {
-
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
--- core/src/main/java/at/peckventure/menu/Settings.java
+++ core/src/main/java/at/peckventure/menu/Settings.java
@@ -1,56 +1,108 @@
 package at.peckventure.menu;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 
-public class Settings implements Screen
-{
+public class Settings implements Screen {
     private Game game;
+    private Stage stage;
+    private Texture backgroundTexture;
+    private Image backgroundImage;
+    private Label titleLabel;
 
-    public Settings(Game game)
-    {
+    public Settings(Game game) {
         this.game = game;
     }
 
     @Override
-    public void show()
-    {
+    public void show() {
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
 
+        // 🎨 Hintergrundbild
+        backgroundTexture = new Texture("textures/background/forest.png");
+        backgroundImage = new Image(backgroundTexture);
+        backgroundImage.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage.addActor(backgroundImage);
+
+        // 📌 Titel
+        Label.LabelStyle titleStyle = new Label.LabelStyle();
+        titleStyle.font = new BitmapFont();
+        titleLabel = new Label("Einstellungen", titleStyle);
+        titleLabel.setFontScale(2f);
+        titleLabel.setAlignment(Align.center);
+
+        // 📌 Infotext
+        Label.LabelStyle textStyle = new Label.LabelStyle();
+        textStyle.font = new BitmapFont();
+        Label infoLabel = new Label("Hier kannst du deine Spieleinstellungen anpassen.", textStyle);
+        infoLabel.setFontScale(1.2f);
+        infoLabel.setAlignment(Align.center);
+
+        // 🔘 Button-Stil
+        Texture buttonTexture = new Texture("textures/gui/button1.png");
+        TextureRegionDrawable buttonDrawable = new TextureRegionDrawable(buttonTexture);
+        TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
+        buttonStyle.up = buttonDrawable;
+        buttonStyle.down = buttonDrawable;
+        buttonStyle.font = new BitmapFont();
+
+        // 📌 Zurück-Button
+        TextButton backButton = new TextButton("Zurück", buttonStyle);
+
+        // 📌 Layout-Tabelle
+        Table rootTable = new Table();
+        rootTable.setFillParent(true);
+        rootTable.top();
+        rootTable.add(titleLabel).padTop(50).expandX().center();
+        rootTable.row();
+        rootTable.add(infoLabel).padTop(20).expandX().center();
+        rootTable.row();
+        rootTable.add(backButton).size(300, 80).padTop(50);
+
+        stage.addActor(rootTable);
+
+        // 📌 Button Event
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new MainMenu(game)); // 🎯 Geht zurück ins Hauptmenü
+            }
+        });
     }
 
     @Override
-    public void render(float v)
-    {
-
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
-    public void resize(int i, int i1)
-    {
-
-    }
+    public void resize(int i, int i1) {}
 
     @Override
-    public void pause()
-    {
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume()
-    {
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide()
-    {
-
-    }
+    public void hide() {}
 
     @Override
-    public void dispose()
-    {
-
+    public void dispose() {
+        backgroundTexture.dispose();
+        stage.dispose();
     }
 }
